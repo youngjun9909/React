76e4b3ed0b801db056ea8e42b65c7f1cbde16868
youngjun9909 <165034190+youngjun9909@users.noreply.github.com>
2024-08-06 09:08:12 +0900

    Add files via upload

---
+++ 0703/a_Effects/Effects03.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+export default function Effects03() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('박영준');
+
+  // 마운팅 시 실행
+  useEffect(() => {
+    console.log(`Component mounted`);
+
+    return () => {
+      console.log(`Component will unmounted`);
+    }
+  }, []);
+
+  // count가 업데이트될 때 실행
+  useEffect(() => {
+    console.log(`Count is ${count}`);
+  }, [count]);
+
+  // name이 업데이트될 때 실행
+  useEffect(() => {
+    console.log(`Name is ${name}`);
+  }, [name]);
+
+  // 모든 렌더링마다 실행
+  // : useEffect를 작성하지 않은 것과 동일한 효과
+  useEffect(() => {
+    console.log('컴포넌트가 리렌더링 됩니다.');
+  });
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment Count
+      </button>
+
+      <p>Name: {name}</p>
+      <button onClick={() => setName(
+        name === '박영준' ? '홍길동' : '박영준'
+      )}>
+        Change Name
+      </button>
+    </div>
+  );
+}
---
+++ 0703/b_Practice/Practice03.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from 'react'
+
+export default function Practice03() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if(inputRef.current) {
+            inputRef.current.focus();
+        }
+    },[])
+
+    return (
+        <div>
+            <input type="text" 
+            ref={inputRef} 
+            placeholder='마운트될 때 포커싱'/>
+        </div>
+    );
+}
