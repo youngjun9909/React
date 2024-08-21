9d315995bfe2bacfda4072787a31e575f5e2a115
youngjun9909 <165034190+youngjun9909@users.noreply.github.com>
2024-08-21 21:25:37 +0900

    Add files via upload

---
+++ 0710/a_Event/Event05.tsx
@@ -0,0 +1,16 @@
+import React, { useCallback, useState } from 'react'
+
+export default function Event05() {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => {
+    setCount(prevCount => prevCount + 1)
+  },[]);
+
+
+  return (
+    <div>
+    <button onClick={increment}>Count: {count}</button>
+    </div>
+  )
+}
